4cbde702992977a519a2a2e2ec2dbc5851e2aebc
Elijah Zolduoarrati <alamo248@student.otago.ac.nz>
2019-10-08 16:56:53 +1300

    repo_total_commits

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -3,7 +3,14 @@
 --@General Scripts
 --***************************************************************
 --creating repo_country again with repo_names
-select pr.id, u.country_code, pr.name into repo_country
-from users u
-join projects pr on u.id = pr.owner_id
-order by pr.id
+--select pr.id, u.country_code, pr.name into repo_country
+--from users u
+--join projects pr on u.id = pr.owner_id
+--order by pr.id
+--***************************************************************
+--creating repo_commits
+select rc.id, rc.country_code, rc.name, count_big(c.id) as total_commits
+from repo_country rc
+join commits c on rc.id = c.project_id
+group by rc.id, rc.country_code, rc.name
+order by total_commits desc
